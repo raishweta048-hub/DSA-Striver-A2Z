f0fd88fa13851a6abbeeed5125cadba033637a4d
Shweta <raishweta048@gmail.com>
2026-02-11 23:06:43 +0530

    Update and rename basic_mathematics.md to basic_mathematics.cpp

--- Basics/basic_mathematics.cpp
+++ Basics/basic_mathematics.cpp
@@ -1,6 +1,6 @@
-# Basic Mathematics for DSA
+//# Basic Mathematics for DSA
 
-## Topics Covered
+/*## Topics Covered
 - Count Digits
 - Reverse a Number
 - Palindrome Check
@@ -15,7 +15,7 @@ Step 1: Take the given number and apply number modulo 10 to extract the last dig
 Step 2: Divide the given number by 10 and ignore the decimal part.
 Continue with these steps by extracting digits and moving ahead by dividing
 it with 10 until you no longer can divide further.
- Then you will be having the given number in reversed order.
+ Then you will be having the given number in reversed order. */
 
  #include<iostream>
 using namespace std;
@@ -43,4 +43,4 @@ int count(int n){
     int cnt = (int)(log10(n) + 1);
     return cnt;
 }
-Time Complexity : O(log10 (N))
+//Time Complexity : O(log10 (N))
