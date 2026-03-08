f9642d8ead46a4f43b630abb6db622c43336f7d5
Shweta <raishweta048@gmail.com>
2026-03-08 20:25:39 +0530

    Update basic_mathematics.cpp

--- Basics/basic_mathematics.cpp
+++ Basics/basic_mathematics.cpp
@@ -64,5 +64,106 @@ int main
     cout<< revNum;
 }
 
-3.Check Palindrome : The numbers for which Reverse is exactly same as 
+/*3.Check Palindrome : The numbers for which Reverse is exactly same as 
 the original one.
+ eg:- 121 */
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value :"<<endl;
+    cin>>n;
+    int revNum= 0;
+    int dup = n;
+    while(n>0) {
+        int ld = n % 10;
+        revNum = (revNum * 10) + ld;
+        n = n / 10; 
+    }
+    if(dup == revNum) cout<<dup<< " is a Palindrome";
+    else cout << "is not a Palindrome";
+    return 0;
+}
+
+/* 4.Armstrong number:-
+Eg :- 371 = 3*3*3 + 7*7*7 + 1*1*1 */
+ 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the value :" <<endl;
+    cin>>n;
+
+    int original = n;
+    int sum = 0;
+
+    while(n > 0) {
+        int ld = n % 10;
+        sum = sum + (ld * ld * ld);
+        n = n / 10;
+    }
+
+    if(sum == original)
+        cout<<"Armstrong Number";
+    else
+        cout<<"Not Armstrong";
+
+    return 0;
+}
+
+/* 5.Factors */
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cout<<"Enter the Value :";
+    cin>>N;
+
+    cout<<"Factors are:"<<endl;
+
+    for(int i = 1; i<=N ; i++){
+        if (N % i == 0){
+            cout<<i<<endl;
+        }
+    }
+
+    return 0;
+}
+/*     Other method:- */
+
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+
+int main(){
+
+    int N;
+    cout<<"Enter number: ";
+    cin>>N;
+
+    vector<int> ls;
+
+    for(int i = 1; i * i < = (N) ; i++){
+        if (N % i == 0){
+            ls.push_back(i);
+
+            if((N/i) != i){
+                ls.push_back(N/i);
+            }
+        }
+    }
+
+    sort(ls.begin() , ls.end());
+
+    for(auto it : ls)
+        cout<< it<<" ";
+
+    return 0;
+}
